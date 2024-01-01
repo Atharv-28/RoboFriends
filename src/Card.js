2862ffa96c03a62234700969a5d7696bcce48093
Atharv-28 <atharvmt@outlook.com>
2024-01-01 20:47:29 +0530

    basic card scrap

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return(
+        <div>
+            <img src="https://robohash.org/ + {props.name}"  alt="img" />
+            <div>
+                <h2>AThar</h2>
+                <p>I am a robo</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
